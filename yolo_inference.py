9d27e5287c306a25eb1760f0813b8f6466fa5d86
vaishnavnegi <vaishnavnegi207@gmail.com>
2024-07-15 16:11:40 +0200

    Updated gitignore. Addded ipynb to fine tune yolo v5 to detect tennis balls using a dataset from robo flow. Running infernce using yolov8x in the meanwhile.

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,5 +1,9 @@
-from ultralytics import YOLO
+from ultralytics import YOLO 
 
 model = YOLO('yolov8x')
 
-model.predict('input_videos/image.png', save = True)
+result = model.predict('input_videos/input_video.mp4',conf=0.2, save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
